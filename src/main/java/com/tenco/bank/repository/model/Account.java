4ffaa15d46bf438bd7de2af239939c4e7b93d887
haejujung <167273792+haejujung@users.noreply.github.com>
2024-08-05 11:25:56 +0900

    firstcommit

---
+++ src/main/java/com/tenco/bank/repository/model/Account.java
@@ -0,0 +1,28 @@
+package com.tenco.bank.repository.model;
+
+import java.security.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Account {
+	private Integer id;
+	private String number;
+	private String password;
+	private Long balance;
+	private Integer userId;
+	private Timestamp createdAt;
+	
+	public void withdraw(Long amount) {
+		this.balance -= amount;
+	}
+
+}
