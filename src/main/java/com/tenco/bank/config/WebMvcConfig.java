23634ec1ee1378f6d4d9551fe276e9731c22f065
haejujung <167273792+haejujung@users.noreply.github.com>
2024-08-13 12:47:37 +0900

    비밀번호암호화처리완료

--- src/main/java/com/tenco/bank/config/WebMvcConfig.java
+++ src/main/java/com/tenco/bank/config/WebMvcConfig.java
@@ -1,7 +1,10 @@
 package com.tenco.bank.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,6 +31,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 			.addPathPatterns("/auth/**");
 	}
 	
-	
+	@Bean // IOC 대상 (싱글톤 처리)
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
--- src/main/webapp/WEB-INF/view/user/signIn.jsp
+++ src/main/webapp/WEB-INF/view/user/signIn.jsp
@@ -11,7 +11,7 @@
 	<form action="/user/sign-in" method="post">
 		<div class="form-group">
 			<label for="username">username:</label>
-			<input type="text" class="form-control" placeholder="Enter username" id="username" name="username" value="길동">
+			<input type="text" class="form-control" placeholder="Enter username" id="username" name="username" value="렌고쿠">
 		</div>
 		<div class="form-group">
 			<label for="pwd">Password:</label>
